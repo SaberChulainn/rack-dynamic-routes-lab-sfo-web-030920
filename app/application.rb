49c91d07ea880653477938cb8266c050c0c35fd5
Ty Bandachit <tbandachit@gmail.com>
2020-03-30 18:26:48 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/item/)
+      item = 
+    else
+  end
+  
+  
+  
+end
