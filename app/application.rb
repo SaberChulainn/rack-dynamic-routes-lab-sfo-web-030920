13e30ad455dacaa787b7dad5ce5f74698b2dd10a
Ty Bandachit <tbandachit@gmail.com>
2020-03-30 18:38:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
     if req.path.match(/items/)
     items_name = req.path.split("/items/").last 
-      item = @@items{|i| i.title == items_name}
+      item = @@items.find{|i| i.title == items_name}
       resp.write item.price
       resp.status = 200
     else
