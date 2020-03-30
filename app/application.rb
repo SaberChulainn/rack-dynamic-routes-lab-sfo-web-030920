4c4636e45eaf9eda9f89779008b53785c5a73a9c
Ty Bandachit <tbandachit@gmail.com>
2020-03-30 18:38:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,11 @@ class Application
     
     if req.path.match(/items/)
     items_name = req.path.split("/items/").last 
-      item = @@items.find{|i| i.name == items_name}
+      if!@@items.find{|i| i.name == items_name}
       resp.write item.price
       resp.status = 200
+    else
+      
     else
      resp.write "Route not found"
      resp.status = 404
